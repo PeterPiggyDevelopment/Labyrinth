b934724502433bd5fe6be49127bda9e4e08daa38
Kirill <deadwolfst@gmail.com>
2016-01-15 15:39:58 +0300

    some warnings fixed in haskell part of server

--- Makefile
+++ Makefile
@@ -1,7 +1,8 @@
 GHC=ghc
 CC=gcc
-CFLAGS=-ggdb -ansi -pedantic -Wall -Werror
-HFLAGS=
+CFLAGS=-ggdb -ansi -pedantic -Wall -Werror 
+SERVCFLAGS=-ggdb -ansi -pedantic -Wall -Werror -O3 -c
+HFLAGS= -O3
 
 main: clean build_s build_c build_f
 
@@ -9,9 +10,9 @@ build_s: build_f
 	$(GHC) $(HFLAGS) src/server/server.hs build/find_way.o build/serialization.o build/list_funcs.o -o build/server
 
 build_f:
-	$(CC) $(CFLAGS) -c src/server/find_way.c -o build/find_way.o
-	$(CC) $(CFLAGS) -c src/server/serialization.c -o build/serialization.o
-	$(CC) $(CFLAGS) -c src/server/list_funcs.c -o build/list_funcs.o
+	$(CC) $(SERVCFLAGS) src/server/find_way.c -o build/find_way.o
+	$(CC) $(SERVCFLAGS) src/server/serialization.c -o build/serialization.o
+	$(CC) $(SERVCFLAGS) src/server/list_funcs.c -o build/list_funcs.o
 
 build_c:
 	$(CC) $(CFLAGS) src/client/net_client.c src/client/eller_lab.c src/client/list_funcs.c -o build/client
--- src/server/server.hs
+++ src/server/server.hs
@@ -1,30 +1,18 @@
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ForeignFunctionInterface #-}
  
 import Data.ByteString.Char8 (pack,  unpack)
-import System.Exit
 import Network hiding (accept, sClose)
 import Network.Socket hiding (recv)
 import Network.Socket.ByteString (sendAll, recv)
 import Control.Concurrent
 import Foreign.C
-import Foreign.Marshal.Alloc
+import Foreign.Marshal.Alloc()
 
 foreign import ccall start_finding:: CString -> CString
  
 main :: IO a
-main = withSocketsDo $ do --forkIO comandListen
-    sock <- listenOn $ PortNumber 5002
-    loop sock
-    where
-        comandListen = do
-            com <- getLine
-            if com == "exit" then exitSuccess
-            else do 
-                putStrLn "Invalid Invalidovich"
-                comandListen
- 
-{-# INLINE loop #-}
+main = withSocketsDo (listenOn (PortNumber 5002) >>= loop)
+
 loop :: Socket -> IO b
 loop sock = do
    (conn, _) <- accept sock
@@ -32,7 +20,7 @@ loop sock = do
    loop sock
    where
       body conn = do
-              line <- start_finding <$> (( unpack <$> (recv conn 1024)) >>= (newCString))
+              line <- start_finding <$> (( unpack <$> recv conn 1024) >>= newCString)
               lint <- pack <$> peekCString line
-              sendAll conn $ lint 
+              sendAll conn lint 
               sClose conn
